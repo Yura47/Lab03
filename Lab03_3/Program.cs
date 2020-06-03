e832de59bf30e8f51a622b64ceb629b32257ef51
Yura <rybakyra@gmail.com>
2020-06-03 12:35:49 +0300

    lab03_3

---
+++ Lab03_3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using System.IO;
+
+
+
+namespace lab03_3
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            StreamReader file = new StreamReader("input.txt");
+
+
+
+            string[] wrds = file.ReadToEnd().Split(new char[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+
+            for (int i = 0; i < wrds.Length; ++i)
+
+            {
+
+                if (wrds[i] == wrds[i].ToLower())
+
+                {
+
+                    wrds[i] = wrds[i].Remove(0);
+
+                }
+
+            }
+
+
+
+            StreamWriter newFile = File.CreateText("output.txt");
+
+
+
+            foreach (string word in wrds)
+
+            {
+
+                if (!string.IsNullOrWhiteSpace(word))
+
+                {
+
+                    newFile.Write("{0} ", word);
+
+                }
+
+            }
+
+
+
+            newFile.Close();
+
+        }
+
+    }
+
+}
